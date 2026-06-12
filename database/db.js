dcbbebadeeac843a5746fed39432165fdc35a326
nitesh-1-official <niteshkumarofficial372@gmail.com>
2026-06-12 12:27:11 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,8 +1,9 @@
+
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 const dbPath = path.join(__dirname, "gym.db");
-
+console.log("DB PATH:", dbPath);
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error(err.message);
@@ -10,7 +11,6 @@ const db = new sqlite3.Database(dbPath, (err) => {
     console.log("Connected to SQLite database");
   }
 });
-
 db.serialize(() => {
 
   db.run(`
@@ -24,6 +24,25 @@ db.serialize(() => {
       deleted INTEGER DEFAULT 0
     )
   `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS payments (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      memberId INTEGER,
+      memberName TEXT,
+      plan INTEGER,
+      amount INTEGER,
+      paymentDate TEXT
+    )
+  `, (err) => {
+
+    if (err) {
+      console.log("PAYMENTS TABLE ERROR:", err.message);
+    } else {
+      console.log("PAYMENTS TABLE READY");
+    }
+
+  });
 
 });
 
